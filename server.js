1d404eeec97e3af90e00cb233a8e217ec60ab7f2
Lovisa Eriksson <lovisa@ahna.se>
2022-09-09 10:39:36 +0200

    US_17, configured swagger in server.js

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require("express");
-const cors = require('cors')
-const app = express();
-const PORT = process.env.PORT || 5001;
-const dotenv = require("dotenv");
+const cors    = require('cors')
+const app     = express();
+const PORT    = process.env.PORT || 5001;
+
+const dotenv   = require("dotenv");
 const mongoose = require("mongoose");
-const registerLoginRoute = require("./routes/registerLogin");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
+
+const registerLoginRoute  = require("./routes/registerLogin");
+const userRoute           = require("./routes/users");
+const postRoute           = require("./routes/posts");
+
+const swaggerJsDoc  = require('swagger-jsdoc')
+const swaggerUi     = require('swagger-ui-express')
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: { 
+      title: "API for Squeak", 
+      description: "Information about the API and the valid HTTP methods",
+      version: "0.0.0 "
+      },
+    servers: [
+      {
+        url: "http://localhost:5001"
+      }
+    ]
+  },
+  apis: [ "./routes/*.js" ]
+}
+
+const swaggerDocument = swaggerJsDoc(swaggerOptions)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 dotenv.config();
 app.use(express.json());
