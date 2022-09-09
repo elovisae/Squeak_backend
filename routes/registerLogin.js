ced4e9ceda9116562ff105f6333781fed0456a15
Lovisa Eriksson <lovisa@ahna.se>
2022-09-09 11:08:13 +0200

    US_17, Documentation for reg/login, done for now

--- routes/registerLogin.js
+++ routes/registerLogin.js
@@ -2,17 +2,100 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+/**
+ * @openapi
+ *   tags:
+ *     name: Users
+ *     description: Managing users
+ */
+
+/**
+ * @openapi
+ *  /api/auth/register:
+ *   post:
+ *     summary: Creates a new user
+ *     description: This POST creates a new user. You need to send in a request body. An example can be found below
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name.
+ *                 example: Jon Doe
+ *               username:
+ *                 type: string
+ *                 description: The user's username.
+ *                 example: jondoe1
+ *               email:
+ *                 type: string
+ *                 description: The user's email.
+ *                 example: jon.doe@some.where
+ *               password:
+ *                 type: string
+ *                 description: The user's password.
+ *                 example: secret
+ *               phone:
+ *                 type: string
+ *                 description: The user's phone.
+ *                 example: 0712387383
+ *     responses:
+ *       200:
+ *         description: Creates a user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: integer
+ *                       description: The user ID.
+ *                       example: 1
+ *                     name:
+ *                       type: string
+ *                       description: The user's name.
+ *                       example: Jon Doe
+ *                     username:
+ *                       type: string
+ *                       description: The user's username.
+ *                       example: jondoe1
+ *                     email:
+ *                       type: string
+ *                       description: The user's email.
+ *                       example: jon.doe@some.where
+ *                     password:
+ *                       type: string
+ *                       description: The user's password.
+ *                       example: secret
+ *                     phone:
+ *                       type: string
+ *                       description: The user's phone.
+ *                       example: 0712387383
+ *       500:
+ *         description: Hint, might already exist a username with that email or username
+ */
+
 //Register
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
     const anomPassword = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
+      name: req.body.name,
       username: req.body.username,
       email: req.body.email,
       password: anomPassword,
+      phone: req.body.phone
+      
     });
-
+    
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
@@ -20,8 +103,36 @@ router.post("/register", async (req, res) => {
   }
 });
 
-//login
+/**
+ * @openapi
+ *  /api/auth/login:
+ *   post:
+ *     summary: Log in as an existing user
+ *     description: This POST lets you log in as an existing user.
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The user's username.
+ *                 example: jondoe1
+ *               password:
+ *                 type: string
+ *                 description: The user's password.
+ *                 example: secret
+ *     responses:
+ *       200:
+ *         description: Succesfully logged in.
+ *       400:
+ *         description: User not found or wrong password
+ */
 
+//login
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
