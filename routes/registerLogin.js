7b8e506eb5d38921d55d827e3207c62d69cb47e3
Johan Blomberg <JohanBlomberg89@gmail.com>
2022-10-07 09:23:23 +0200

    fixed key for when a user registers

--- routes/registerLogin.js
+++ routes/registerLogin.js
@@ -96,7 +96,7 @@ router.post("/register", async (req, res) => {
     });
 
     const user = await newUser.save();
-    res.status(200).json(user);
+    res.status(200).json({user, loggedIn: true});
   } catch (err) {
     res.status(500).json(err);
   }
